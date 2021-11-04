21b8b6d933fb7e5f5c3c2aa89b21497ebaa73782
Dmytro Demianenko <83134601+DmytroDemianenko@users.noreply.github.com>
2021-11-04 23:44:41 +0100

    1-st

--- task-1.js
+++ task-1.js
@@ -8,3 +8,4 @@ const delay = ms => {
   delay(2000).then(logger); // Resolved after 2000ms
   delay(1000).then(logger); // Resolved after 1000ms
   delay(1500).then(logger); // Resolved after 1500ms
+
